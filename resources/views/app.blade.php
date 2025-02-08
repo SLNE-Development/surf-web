a7e6fdee6291bbfd6e7b8450c6d88c0fa692a748
Ammo <ammo@slne.dev>
2025-02-08 23:20:03 +0100

    add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="icon" type="image/png" href="/images/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/images/favicon/favicon.svg" />
+    <link rel="shortcut icon" href="/images/favicon/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="Arty" />
+    <link rel="manifest" href="/images/favicon/site.webmanifest" />
+
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
