489c832e202b97677a1dbd7adcfd172e5223d045
Ammo <ammo@slne.dev>
2025-02-08 23:58:54 +0100

    fix casing in page component paths for consistency

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -11,7 +11,7 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) =>
         resolvePageComponent(
-            `./Pages/${name}.tsx`,
+            `./pages/${name}.tsx`,
             import.meta.glob("./pages/**/*.tsx")
         ),
     setup({ el, App, props }) {
