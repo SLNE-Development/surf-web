833c18041a1906739d36c958aefe9a04a2fd6c79
Ammo <ammo@slne.dev>
2025-03-03 12:40:56 +0100

    feat: enhance application security and performance in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +23,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Vite::useAggressivePrefetching();
         Vite::prefetch(concurrency: 3);
+
+        Model::shouldBeStrict();
+        DB::prohibitDestructiveCommands($this->app->isProduction());
+        URL::forceScheme("https");
+
     }
 }
